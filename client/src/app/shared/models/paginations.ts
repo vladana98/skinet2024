eddb63b027eae0ae0be41eb9817f78cb15a11777
vladana98 <vladanaveselinovic@yahoo.com>
2024-09-15 19:53:49 +0200

    End of section 8

---
+++ client/src/app/shared/models/paginations.ts
@@ -0,0 +1,9 @@
+import { NumberLiteralType } from "typescript";
+
+export type Pagination<T> = {
+pageIndex: number;
+pageSize: number;
+count: number;
+data: T[]
+
+}
---
+++ client/src/app/shared/models/product.ts
@@ -0,0 +1,13 @@
+export type Product = {
+    id: number;
+    name: string; 
+    description: string;
+    price: number;
+    pictureUrl: string;
+    type: string;
+    brand: string;
+    quantityInStock: number;
+    
+
+
+}
