dd850d3bd5845d6cec71a61e4f68b22ff3def779
vladana98 <vladanaveselinovic@yahoo.com>
2024-10-13 19:11:40 +0200

    End of section 14

---
+++ API/Extensions/ClaimsPrincipleExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Core.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using System.Security.Authentication;
+using System.Security.Claims;
+
+namespace API.Extensions
+{
+    public static class ClaimsPrincipleExtensions
+    {
+        public static async Task<AppUser> GetUserByEmail(this UserManager<AppUser> userManager, ClaimsPrincipal user)
+        {
+            var userToRetturn = await userManager.Users.FirstOrDefaultAsync(x => x.Email == user.GetEmail());
+
+            if (userToRetturn == null)
+                throw new AuthenticationException("User not found.");
+            return userToRetturn;
+        }
+        public static async Task<AppUser> GetUserByEmailWithAddress(this UserManager<AppUser> userManager, ClaimsPrincipal user)
+        {
+            var userToRetturn = await userManager.Users
+                .Include(x => x.Address)
+                .FirstOrDefaultAsync(x => x.Email == user.GetEmail());
+
+            if (userToRetturn == null)
+                throw new AuthenticationException("User not found.");
+            return userToRetturn;
+        }
+        public static string GetEmail(this ClaimsPrincipal user)
+            {
+                {
+                    var email = user.FindFirstValue(ClaimTypes.Email);
+                    if (email == null) throw new AuthenticationException("Email nije pronađen.");
+                    return email;
+                }
+            }
+        
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Middleware;
+using Core.Entities;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Infrastructure.Services;
@@ -29,15 +30,22 @@ builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
 
 builder.Services.AddSingleton<ICartService, CartService>();
 
+builder.Services.AddAuthorization();
+builder.Services.AddIdentityApiEndpoints<AppUser>()
+    .AddEntityFrameworkStores<StoreContext>();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseMiddleware<ExceptionMiddleware>();
 
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200","https://localhost:4200"));
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
+.WithOrigins("http://localhost:4200","https://localhost:4200"));
 
 app.MapControllers();
+app.MapGroup("api").MapIdentityApi<AppUser>(); // api/login
 
 try
 {
