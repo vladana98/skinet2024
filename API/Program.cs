1796cd99c21252e394cab7bf85e9494820abf9a2
vladana98 <vladanaveselinovic@yahoo.com>
2024-10-19 21:47:29 +0200

    CORS FIXED

--- API/Program.cs
+++ API/Program.cs
@@ -8,7 +8,23 @@ using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddCors(
+    (setup) =>
+    {
+        setup.AddPolicy(
+            "CorsPolicy",
+            (options) =>
+            {
+                options
+                    .WithOrigins(["https://localhost:4200"])
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+            }
+        );
+    }
+);
+
 
 builder.Services.AddControllers();
 builder.Services.AddDbContext<StoreContext>(opt =>
@@ -18,7 +34,7 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-builder.Services.AddCors();
+
 builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
 {
     var connString = builder.Configuration.GetConnectionString("Redis");
@@ -35,17 +51,23 @@ builder.Services.AddIdentityApiEndpoints<AppUser>()
     .AddEntityFrameworkStores<StoreContext>();
 
 
+
+
 var app = builder.Build();
+
+app.UseCors("CorsPolicy");
+
 
 // Configure the HTTP request pipeline.
 
 app.UseMiddleware<ExceptionMiddleware>();
 
-app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
-.WithOrigins("http://localhost:4200","https://localhost:4200"));
+
+
 
 app.MapControllers();
 app.MapGroup("api").MapIdentityApi<AppUser>(); // api/login
+
 
 try
 {
